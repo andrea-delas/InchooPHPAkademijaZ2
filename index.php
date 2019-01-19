c242747f432cbe3df9b42c49aae8a0142508ce13
andrea-delas <andrea.delas@outlook.com>
2019-01-19 16:29:38 +0100

    Izračun aritmetičke sredine i prvog većeg parnog broja

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<?php
-$myarray = array(2,6,49,3,2,3,4,50,33,34,37,42,55,234,122);
-sort($myarray); //sortiranje od najmanjeg prema najvećem
 
-$arrlength = count($myarray);
-foreach($myarray as $i)
+<?php
+$myArray = array(2,6,49,3,2,3,4,50,33,34,37,42,55,234,122);
+
+sort($myArray); //sortiranje od najmanjeg prema najvećem
+
+
+$arrLength = count($myArray);
+foreach($myArray as $i)
 {
     if(($i%2) == 0 ){  //izdvajanje samo parnih brojeva
         echo $i;
@@ -11,12 +14,22 @@ foreach($myarray as $i)
     }
 }
 
-for($i = 0; $i < $arrlength; $i++) {
 
+$averageOfMyArray = array_sum($myArray) / count($myArray); //izračun aritmetičke sredine
+
+echo "Aritmetička sredina: " . $averageOfMyArray;
+echo "<br>";
+
+$nextNumber = null;
+foreach ($myArray as $key => $value) {
+    if ($value >= $averageOfMyArray && $value % 2 === 0) {  //izračun prvog većeg parnog broja od aritmetičke sredine
+        $nextNumber = $value;
+        break;
+    }
 }
+echo "Prvi veći parni broj: " . $nextNumber, "<br />";
 
-$average_of_myarray = array_sum($myarray) / count($myarray);
-echo $average_of_myarray;
-
+echo sqrt(sqrt(2));
 
 ?>
+
